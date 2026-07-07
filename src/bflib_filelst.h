65c82bfec4b711b059c5bed1f453ef2d865fb7a4
Matthew Deaves <mndeaves@me.com>
2026-07-07 23:28:28 +0100

    Portability: unpack in-memory-only packed structs, broaden unix guard
    Drop #pragma pack(1) from structs that only ever live in memory (never serialized to disk or the network), so they no longer trigger unaligned-access faults on arm64, and broaden a non-Linux/unix guard. In-memory layout only; Windows/serialized structs are untouched.
    
    (cherry picked from commit 47a472c062cca38a3fea37375fea14bcf6c52bba)

--- src/bflib_filelst.h
+++ src/bflib_filelst.h
@@ -26,8 +26,6 @@
 extern "C" {
 #endif
 /******************************************************************************/
-#pragma pack(1)
-
 struct TbLoadFiles;
 
 typedef const char * ModifyDataLoadFnameFunc(const char *);
@@ -51,9 +49,7 @@ struct TbLoadFilesV2 {
     LoadFilesGetSizeFunc GetSizeFunc;
     LoadFilesUnpackFunc UnpackFunc;
 };
-
-#pragma pack()
-/******************************************************************************/
+/*****************************************************************************/
 const char * defaultModifyDataLoadFilename(const char *);
 ModifyDataLoadFnameFunc *LbDataLoadSetModifyFilenameFunction(ModifyDataLoadFnameFunc *newfunc);
 extern ModifyDataLoadFnameFunc *modify_data_load_filename_function;
